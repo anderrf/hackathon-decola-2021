b326a10d3f7956bd34c8d548e18f68d936fe9418
anderrf <anderson_rocha_farias@outlook.com>
2021-10-02 11:08:01 -0300

    Remoção de arquivos padrão

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Inicio from '../views/Inicio'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: Home
-  },
-  {
-    path: '/about',
-    component: About
+    component: Inicio,
+
   }
 ]
 
---
+++ src/views/Inicio.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+        <h1>Início</h1>
+    </v-container>
+</template>
+<script>
+    export default {
+        name: 'Inicio'
+    };
+</script>
+<style scoped>
+
+</style>
