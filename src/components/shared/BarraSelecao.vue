198cf525f52505928c61ae0abd7c8c2d6b8d9b73
Gian Luca Oliveira Ribeiro e Silva <gian.silva@decola.iteris.com.br>
2021-10-02 14:59:21 -0300

    Barra de seleção fixada no final da página

--- src/components/shared/BarraSelecao.vue
+++ src/components/shared/BarraSelecao.vue
@@ -1,22 +1,29 @@
 <template>
-    <v-toolbar color="#FF7315" class="barra-selecao">
-        <v-btn fab color="#F4F4f4" class="my-3">
-            <v-icon color="#232020">mdi-television-classic</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn fab color="#F4F4f4" class="my-3">
-            <v-icon color="#232020">mdi-controller-classic-outline</v-icon>
-        </v-btn>
-    </v-toolbar>
+  <v-toolbar color="#FF7315" class="barra-selecao">
+    <v-btn fab color="#F4F4f4" class="my-3">
+      <v-icon color="#232020">mdi-television-classic</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn fab color="#F4F4f4" class="my-3">
+      <v-icon color="#232020">mdi-controller-classic-outline</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
 <script>
-    export default {
-        name: 'BarraSelecao'
-    }
+export default {
+  name: "BarraSelecao",
+};
 </script>
 <style scoped>
-    .my-3 {
-        width: 35px;
-        height: 35px;
-    }
+.barra-selecao {
+  position: fixed;
+  text-align: center;
+  bottom: 0px;
+  width: 100%;
+}
+
+.my-3 {
+  width: 35px;
+  height: 70px;
+}
 </style>
